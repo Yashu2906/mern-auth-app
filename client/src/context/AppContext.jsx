9545616bd31408942ea97e70828bd3ef63d7f590
Yashu2906 <144900536+Yashu2906@users.noreply.github.com>
2025-08-13 00:00:38 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 export const AppContextProvider = (props) => {
   axios.defaults.withCredentials = true;
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://authentication-backend-5u6n.onrender.com";
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(false);
 
